6241c46fa7205365ace18567443ae148633e1cbb
Alan Curtis <alancurtis5000@gmail.com>
2020-05-06 16:24:16 -0600

    lint wa for destructing props

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import './styles/main.scss';
 import AppRouter from './routes/AppRouter';
-import { updateScreenSize } from './actions/screenSize';
+import { updateScreenSize as actionUpdateScreenSize } from './actions/screenSize';
 
-const App = (props) => {
-  const handleWindowSizeChange = () => {
-    console.log(window.innerWidth);
+export class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
     const width = window.innerWidth;
-    props.updateScreenSize(width);
+    const { updateScreenSize } = this.props;
+    updateScreenSize(width);
   };
 
-  useEffect(() => {
-    // comp mounted
-    console.log('App mounted');
-    window.addEventListener('resize', handleWindowSizeChange);
-  }, []);
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <AppRouter />
+        </header>
+      </div>
+    );
+  }
+}
 
-  useEffect(() => {
-    // comp will unmounted
-
-    return () => {
-      console.log('cleaned up');
-      window.removeEventListener('resize', handleWindowSizeChange);
-    };
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button type="button" onClick={() => console.log(props.screenSize)}>
-          log
-        </button>
-        <AppRouter />
-      </header>
-    </div>
-  );
+const mapActionsToProps = {
+  updateScreenSize: actionUpdateScreenSize,
 };
 
 const mapStateToProps = (state) => {
@@ -44,4 +40,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { updateScreenSize })(App);
+export default connect(mapStateToProps, mapActionsToProps)(App);
